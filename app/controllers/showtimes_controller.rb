446ebe416cc45703bfba9eaab5895a4752ff39da
Lauren Markzon <lmarkzon@gmail.com>
2016-01-22 15:54:48 -0600

    Complete methods and logic for movie theater scheduler and display

--- app/controllers/showtimes_controller.rb
+++ app/controllers/showtimes_controller.rb
@@ -1,38 +1,61 @@
-require 'csv'
 require_relative '../models/movie'
 require_relative '../models/theater'
+require 'csv'
 
 class ShowtimesController
-  def movie_theater_scheduler(filename)
-    @movies_array = []
+  def initialize
     @theater_array = []
-
-    CSV.foreach(filename, :headers => false) do |row|
-      if row[0] == 'Theater'
-        theater_parser(row)
-      else
-        movie_parser(row)
-        end
-      end
-    display_movie_theater_schedule(@theater_array)
+    @movie_array = []
   end
 
-  def movie_parser(row)
-    @movie = Movie.new(movie_title: row[0], theater: row[1], release_year: row[2], mpaa_rating: row[3], runtime: row[4])
-    @movies_array.push(@movie)
-    # puts @movies_array
+  def movie_theater_scheduler(filename)
+    CSV.foreach(filename) do |row|
+      case
+        when row[0] == 'Theater'
+          theater_parser(row)
+        when row[0] == 'Movie'
+          movie_parser(row)
+        when row[0] == 'Set'
+          set_theater_movies(row)
+        else
+          puts "Please enter a file with valid information."
+      end
+    end
+    display_movie_theater_scheduler(@theater_array)
   end
 
   def theater_parser(row)
     @theater = Theater.new(location: row[1], opening_time: row[2], closing_time: row[3])
-    @theater_array.push(@theater)
+    @theater_array << @theater
     # puts @theater_array
   end
 
-  def display_movie_theater_schedule(theater_array)
-    theater_array.each do |array|
-      array.display_location_and_hours
-        array.movies.each {|array| array.print_movie_schedule }
+  def movie_parser(row)
+    @movie = Movie.new(movie_title: row[1], release_year: row[2], mpaa_rating: row[3], runtime: row[4])
+    @movie_array << @movie
+    # puts @movie_array
+  end
+
+  def set_theater_movies(row)
+    @theater_array.each do |theater|
+      if theater.location == row[2]
+        current_movie = @movie_array.find do |movie|
+          movie.movie_title == row[1]
+        end
+        movies_in_theater = theater.movies
+        new_movie = Movie.new(movie_title: current_movie.movie_title, release_year: current_movie.release_year, mpaa_rating: current_movie.mpaa_rating, runtime: current_movie.runtime, theater: theater)
+
+        movies_in_theater << new_movie
+      end
+    end
+  end
+
+  def display_movie_theater_scheduler(theaters)
+    theaters.each do |theater|
+      theater.display_theater_info
+      theater.movies.each do |movie|
+        movie.display_movie_scheduler
+      end
     end
   end
 end
